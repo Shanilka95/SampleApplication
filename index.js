7db8a28724c0148c86478bc94fec29e1faf0a8d8
Shanilka Liyanage <shanilkaliyanage9@gmail.com>
2022-09-11 00:38:47 +0530

    minor change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
+import App from './App';
 import {name as appName} from './app.json';
 import HomeCC from './src/Home.CC';
 import HomeFC from './src/Home.FC';
